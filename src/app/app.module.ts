5f6108e5dc7609270e1ffbbb521ece55ff6a0b2b
Yevhenii.Salamaha <yevhenii.salamaha@ysalamakha-MacBook-Pro.local>
2022-08-09 09:24:22 +0300

    created navbar and main components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MainComponent } from './components/main/main.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    NavbarComponent,
+    
   ],
   imports: [
     BrowserModule
