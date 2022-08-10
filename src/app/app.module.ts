a44287ce0fec17ed597fa00a3e5a18237f0c84ae
Yevhenii.Salamaha <yevhenii.salamaha@ysalamakha-MacBook-Pro.local>
2022-08-10 12:27:13 +0300

    fix: create values for transition and optimize code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { MainComponent } from './components/main/main.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { MainContentComponent } from './components/main-content/main-content.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainComponent,
     NavbarComponent,
+    MainContentComponent,
     
   ],
   imports: [
